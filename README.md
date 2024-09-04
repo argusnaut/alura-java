c1052b6123a32ae99c5a6c0ff287feb0ce959960
Vitor Argus <argusvi@outlook.com>
2024-09-04 13:15:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AluraJava
+Project used to remind and learn from new java versions.
+
+Kanban: [trello](https://trello.com/b/G4EfbCkh/api-voll-med)
---
+++ src/main/java/med/voll/api/address/AddressData.java
@@ -0,0 +1,5 @@
+package med.voll.api.address;
+
+public record AddressData(String street, String neighborhood, String zip_code, String city, String state, String number,
+                          String complement) {
+}
